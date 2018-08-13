5bfac443b7f8156151b71a5e277069c64accf6ab
xing.gex <xing.gex@alibaba-inc.com>
2018-08-13 09:41:17 +0800

    fix: route array support.

--- src/route.jsx
+++ src/route.jsx
@@ -7,7 +7,17 @@ const route = function (path, options) {
     meta.route = true;
     return function withRoute(Component) {
         return class RouterWrapper extends React.Component {
+            renderPath(path) {
+                return path.map(item => {
+                    return <Route {...options} path={item} component={Component}/>
+                });
+            }
             render() {
+                if(Array.isArray(path)) {
+                    return <div>
+                        {this.renderPath(path)}
+                    </div>
+                }
                 return <Route {...options} path={path} component={Component}/>;
             }
         };
