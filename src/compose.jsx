cae5bea40d68b8b8ed0501b10caefc319baf2e95
xing.gex <xing.gex@alibaba-inc.com>
2018-08-15 16:17:43 +0800

    fix: Add compose hooks.

--- src/compose.jsx
+++ src/compose.jsx
@@ -3,27 +3,41 @@ import ReactDOM from 'react-dom';
 import Store from './store';
 import inject from './inject';
 
+const noop = function () {};
+
 export default function compose({
     view,
     components,
     state,
     actions,
     container,
-    init,
+    init = noop,
+    mounted = noop,
+    beforeUpdate = noop,
+    updated = noop,
+    beforeDestroy = noop
 }) {
     const store = new Store({
         state,
         actions
     });
     class ComposeComponent extends React.Component {
-        constructor() {
-            super(...arguments);
-
-            if(typeof(init) === 'function') {
-                init.call(this, ...arguments);
-            }
+        constructor(...args) {
+            super(...args);
+            init.apply(this, args);
         }
-
+        componentDidMount(...args) {
+            mounted.apply(this, args);
+        }
+        componentWillUpdate(...args) {
+            beforeUpdate.apply(this, args);
+        }
+        componentDidUpdate(...args) {
+            updated.apply(this, args);
+        }
+        componentWillUnmount(...args) {
+            beforeDestroy.apply(this, args);
+        }
         render() {
             return view.call(this, {
                 createElement: React.createElement,
