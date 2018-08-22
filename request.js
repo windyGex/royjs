ec7bd0b83a26cf857638542d21fdce72b177a9de
xing.gex <xing.gex@alibaba-inc.com>
2018-08-22 13:21:58 +0800

    feat: Add debug for royjs.

--- request.js
+++ request.js
@@ -1,3 +1,32 @@
+const axios = require('axios');
 const DataSource = require('./lib/data-source');
-DataSource.prototype.request = require('axios');
-module.exports = require('axios');
+
+const isDebugger = () => location.href.indexOf('debug') > -1;
+const url = (pathname) => {
+    if (isDebugger()) {
+        const query = location.search.split('&').reduce((currentObj, item) => {
+            const itemArr = item.split('=');
+            currentObj[itemArr[0]] = itemArr[1];
+            return currentObj;
+        }, {});
+        return `http://rap2api.alibaba-inc.com/app/mock/${query.port}${pathname}`;
+    }
+    return pathname;
+};
+
+axios.interceptors.response.use(
+    function (response) {
+        return response.data;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.request.use(function (config) {
+    config.url = url(config.url);
+    return config;
+});
+
+DataSource.prototype.request = axios;
+module.exports = axios;
