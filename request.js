f09386a7fd69f7a1e04723eafb88f6934a7a45e7
xing.gex <xing.gex@alibaba-inc.com>
2018-11-05 11:30:06 +0800

    fix: compile error

--- request.js
+++ request.js
@@ -1,5 +1,5 @@
-const request = require('@alife/hippo-request');
-const DataSource = require('./lib/data-source');
+var request = require('@alife/hippo-request');
+var DataSource = require('./lib/data-source');
 
 DataSource.prototype.request = request;
 module.exports = request;
