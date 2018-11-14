b2b55880fee51759d18dbfc58d23f11152cbd921
xing.gex <xing.gex@alibaba-inc.com>
2018-11-14 18:05:12 +0800

    fix: Trigger change when using set and any others.

---
+++ test/case.spec.js
@@ -0,0 +1,95 @@
+/* eslint-disable */
+import chai from 'chai';
+import React from 'react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import {Store, inject, connect, Provider, compose} from '../src/index';
+import {JSDOM} from 'jsdom';
+import sinon from 'sinon';
+
+const doc = new JSDOM('<!doctype html><html><body></body></html>');
+global.document = doc.window.document;
+global.window = doc.window;
+
+Enzyme.configure({ adapter: new Adapter() });
+
+chai.expect();
+
+const expect = chai.expect;
+
+describe('support inject store to React Component', () => {
+    let store, App, wrapper;
+    class Demo extends React.Component {
+        render() {
+            const [item] = this.store.state.a;
+            return <span>{item && item.status ? 'true' : 'false'}</span>;
+        }
+    }
+    beforeEach (() => {
+        store = new Store({
+            state: {
+                a: []
+            }
+        });
+        App = inject(store)(Demo);
+        wrapper = mount(<App/>);
+    });
+
+    afterEach(() => {
+        store = null
+        App = null
+        wrapper = null
+    });
+
+    // set;
+    it('should support item change for array', () => {
+        store.state.set('a', [{
+            status: true
+        }]);
+        expect(wrapper.find('span').text()).eql('true');
+        store.state.a[0].set('status', false);
+        expect(wrapper.find('span').text()).eql('false');
+        store.state.set('a', [{
+            status: true
+        }]);
+        expect(wrapper.find('span').text()).eql('true');
+    });
+
+    it('should support array method', () => {
+        expect(wrapper.find('span').text()).eql('false');
+        store.state.a.push({
+            status: true
+        });
+        expect(wrapper.find('span').text()).eql('true');
+        store.state.a[0].set('status', false);
+        expect(wrapper.find('span').text()).eql('false');
+    })
+
+    it('should support set key method', () => {
+        expect(wrapper.find('span').text()).eql('false');
+        store.state.set('a[0].status', true);
+        expect(wrapper.find('span').text()).eql('true');
+        store.state.a[0].set('status', false);
+        expect(wrapper.find('span').text()).eql('false');
+        store.state.set('a[0].status', true);
+        expect(wrapper.find('span').text()).eql('true');
+    })
+
+    it('should support proxy', () => {
+        store.state.a = [{
+            status: true
+        }];
+        expect(wrapper.find('span').text()).eql('true');
+        store.state.a[0].status = false;
+        expect(wrapper.find('span').text()).eql('false');
+    })
+
+    it('should support push proxy', () => {
+        store.state.a.push({
+            status: true
+        });
+        expect(wrapper.find('span').text()).eql('true');
+        store.state.a[0].status = false;
+        expect(wrapper.find('span').text()).eql('false');
+    })
+})
