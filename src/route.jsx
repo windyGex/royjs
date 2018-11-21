3ab0851e0a7e9a5929c82734e219b371698e7f88
xing.gex <xing.gex@alibaba-inc.com>
2018-11-21 15:40:28 +0800

    temp: deprecated route and render method

--- src/route.jsx
+++ src/route.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 /*eslint-disable*/
 import { Route } from 'react-router-dom';
 import meta from './meta';
+import {warning} from './utils';
 
 const route = function(path, options) {
     meta.route = true;
     return function withRoute(Component) {
+        warning('[ route ] method is deprecated and will be removed at next version.');
         return class RouterWrapper extends React.Component {
             renderPath(path) {
                 return path.map(item => {
